a0063d11e33df6b85ff93833fe4fe310ca8be0ee
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-08-20 20:46:38 -0300

    modificacion modelo ciclo

--- backend/auditorias/admin.py
+++ backend/auditorias/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.db import models
+from .models import Ciclo
+
 
 # Register your models here.
+admin.site.register(Ciclo)
--- backend/auditorias/models.py
+++ backend/auditorias/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
+
+class TimeStampedModel(models.Model):
+    creado = models.DateTimeField(default=timezone.now, editable=False)
+    modificado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 # Create your models here.
-class Ciclo(models.Model):
+class Ciclo(TimeStampedModel):
+    codigo = models.CharField(max_length=20)
     nombre = models.CharField(max_length=200)
-    codigo = models.CharField(max_length=20, primary_key=True)
-    creado = models.DateTimeField(auto_created=True)
-    modificado = models.DateTimeField(auto_created=True)
+    padre = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.nombre
