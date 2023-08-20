ec51ccfd50e8d00b78dbd1c88a3e021bf4025f52
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-08-20 20:05:32 -0300

    creacion ciclo

--- backend/auditorias/models.py
+++ backend/auditorias/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Ciclo(models.Model):
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=20, primary_key=True)
+    creado = models.DateTimeField(auto_created=True)
+    modificado = models.DateTimeField(auto_created=True)
