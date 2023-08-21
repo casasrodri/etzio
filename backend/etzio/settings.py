ad908cf6ced0898562f3074c65228354efff1627
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-08-21 16:29:32 -0300

    corheaders allow all

--- backend/etzio/settings.py
+++ backend/etzio/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-CORS_ORIGIN_WHITELIST = ['http://localhost:80']
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ORIGIN_WHITELIST = ['http://localhost:80']
